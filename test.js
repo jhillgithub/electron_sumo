fe3d4b491a08ce0a10747d8c822539b2af93222d
J H <JH@Js-MacBook-Pro.local>
2016-10-22 17:45:27 -0500

    added basic motion controls

--- test.js
+++ test.js
@@ -2,25 +2,52 @@ var sumo = require('node-sumo');
 
 var drone = sumo.createClient();
 
-var forward = function() {
-  drone.connect(function() {
-    drone.postureJumper();
-    drone.forward(50);
+var connect = drone.connect();
 
-    setTimeout(function() {
-      drone.stop();
-    }, 1000);
-  });
+var forward = function() {
+  drone.postureJumper();
+  drone.forward(50);
+
+  setTimeout(function() {
+    drone.stop();
+  }, 1000);
 }
+var backward = function() {
+  drone.postureJumper();
+  drone.backward(50);
+
+  setTimeout(function() {
+    drone.stop();
+  }, 1000);
+}
+var left = function() {
+  drone.postureJumper();
+  drone.left(50);
+
+  setTimeout(function() {
+    drone.stop();
+  }, 100);
+}
+var right = function() {
+  drone.postureJumper();
+  drone.right(50);
+
+  setTimeout(function() {
+    drone.stop();
+  }, 100);
+}
+
 var longJump = function() {
-  drone.connect(function() {
-    drone.postureJumper();
-    drone.drone.animationsLongJump();
-  });
+  drone.postureJumper();
+  drone.animationsLongJump();
 }
 
 var sumo = {
+  connect: connect,
   forward: forward,
+  backward: backward,
+  left: left,
+  right: right,
   longJump: longJump,
   test: function() {console.log("test")}
 }
